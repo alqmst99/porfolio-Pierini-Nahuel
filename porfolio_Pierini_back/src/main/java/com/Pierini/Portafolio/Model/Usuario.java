42e6beb720b94c54bc90f10d0bba3d2624a9d2a6
Nahuel Pierini <nahuel.pierini90@hotmail.com>
2022-08-15 19:23:33 -0300

    portafolio 1.0.0

---
+++ porfolio_Pierini_back/src/main/java/com/Pierini/Portafolio/Model/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Pierini.Portafolio.Model;
+
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Usuario {
+    
+}
