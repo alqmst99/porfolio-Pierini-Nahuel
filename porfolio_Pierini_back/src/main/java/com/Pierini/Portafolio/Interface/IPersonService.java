119d179278ff7b990bf158001a26e6993e1b80bb
Nahuel Pierini <nahuel.pierini90@hotmail.com>
2022-10-07 14:57:00 -0300

    portafolio 1.00.03

--- porfolio_Pierini_back/src/main/java/com/Pierini/Portafolio/Interface/IPersonService.java
+++ porfolio_Pierini_back/src/main/java/com/Pierini/Portafolio/Interface/IPersonService.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.Pierini.Portafolio.Interface;
+
+import com.Pierini.Portafolio.Entity.Person;
+import java.util.List;
 
 /**
  *
- * @author Nahue
+ * @author Pierini Nahuel Nicolas
  */
 public interface IPersonService {
+    //get List
+    public List<Person>getPersons();
+    //save Person
+    public void savePerson(Person person);
+    //delete Person
+    public void deletePerson(Long id);
+    //sarch person
+    public Person findPerson(Long id);
     
 }
